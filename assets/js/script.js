de641d616e5e4840b7431c12ea8d44c337dc6160
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-07-26 21:55:09 +0800

    Fix navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,23 @@ $(document).ready(function(){
         if(!isToggle){
             $("#menu-icon").attr("src", "assets/icons/close.svg")
             isToggle = true;
+            $(".navbar").css({
+                "-webkit-box-shadow": "0px 2px 5px 0px rgba(0,0,0,0.75)",
+                "-moz-box-shadow": "0px 2px 5px 0px rgba(0,0,0,0.75)",
+                "box-shadow": "0px 2px 5px 0px rgba(0,0,0,0.75)"
+            });
         }
         else{
             $("#menu-icon").attr("src", "assets/icons/menu.svg")
             isToggle = false;
+            $(".navbar").css("box-shadow", "none");
         }
     })
+    if(innerWidth < 992){
+        $(".nav-link").click(function(){
+            $(".navbar-toggler").click();
+        })
+    }
     // When download cv is hover over, the style will change
     $(".btn-download-cv").hover(
         function(){
