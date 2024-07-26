815f7ee3c05ea1a08716177da2a0fec09c7b3279
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-07-26 22:55:45 +0800

    Add about page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // alert("Development in progress");
 $(document).ready(function(){
+
     let isToggle = false;
     $(".navbar-toggler").click(function(){
         if(!isToggle){
@@ -17,11 +18,39 @@ $(document).ready(function(){
             $(".navbar").css("box-shadow", "none");
         }
     })
+    // Automatically closed the navbar when any link is clicked
     if(innerWidth < 992){
         $(".nav-link").click(function(){
             $(".navbar-toggler").click();
         })
     }
+    // Change the color of selected link
+    $(".nav-link").click(function(){
+        $(".nav-link").attr("class", "nav-link")
+        $(this).attr("class", "nav-link active");
+        
+        // use this to check the scroll top of each link
+        setTimeout(function(){
+            var scrollTop = $(window).scrollTop();
+            // alert(scrollTop)
+        }, 3000);
+    })
+    $('body').on('wheel', function(event) {
+        // Get the current scroll position
+        var scrollTop = $(window).scrollTop();
+        console.log(scrollTop)
+        if(scrollTop <= 328){
+            $(".nav-link").attr("class", "nav-link")
+            $("#home-link").attr("class", "nav-link active");
+        }
+        else if(scrollTop >= 329){
+            $(".nav-link").attr("class", "nav-link")
+            $("#about-link").attr("class", "nav-link active");
+        }
+        // I just wan to insert a code here that can detect what is the height are we now to determine what page should we focus on
+        event.preventDefault();
+    });
+
     // When download cv is hover over, the style will change
     $(".btn-download-cv").hover(
         function(){
